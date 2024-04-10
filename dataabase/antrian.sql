90825285243628c27b5a1d6e27fc306b0f6ed3e3
Pandepriatama <putupande2003@gmail.com>
2024-04-10 17:40:32 +0800

    menambahkan file database

---
+++ dataabase/antrian.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Apr 2024 pada 11.34
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistem_antrian_dokter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `antrian`
+--
+
+CREATE TABLE `antrian` (
+  `id_antrian` int(11) NOT NULL,
+  `id_pasien` int(11) DEFAULT NULL,
+  `id_dokter` int(11) DEFAULT NULL,
+  `tanggal_periksa` date NOT NULL,
+  `status` enum('Menunggu','Dalam Pemeriksaan','Selesai') DEFAULT 'Menunggu'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `antrian`
+--
+
+INSERT INTO `antrian` (`id_antrian`, `id_pasien`, `id_dokter`, `tanggal_periksa`, `status`) VALUES
+(2, 3, 400, '2024-04-10', 'Selesai'),
+(4, 9, 900, '2024-04-19', 'Selesai');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `antrian`
+--
+ALTER TABLE `antrian`
+  ADD PRIMARY KEY (`id_antrian`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `antrian`
+--
+ALTER TABLE `antrian`
+  MODIFY `id_antrian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
